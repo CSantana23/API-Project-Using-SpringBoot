a90aebf330d682f22c06158c2d7ac2d3ff957779
CSantana23 <cesarsantana475@gmail.com>
2021-10-25 19:44:59 -0400

    Added the ability to connect to our response endpoint using SOAP.

--- src/main/java/com/csantana/soapproject/soapapiproject/soapcoursemanagement/CourseDetailsEndpoint.java
+++ src/main/java/com/csantana/soapproject/soapapiproject/soapcoursemanagement/CourseDetailsEndpoint.java
@@ -1,8 +1,12 @@
 package com.csantana.soapproject.soapapiproject.soapcoursemanagement;
 
+import com.csantana.soapproject.soapapiproject.soap.service.CourseDetailsService;
 import com.in28minutes.courses.CourseDetails;
 import com.in28minutes.courses.GetCourseDetailsRequest;
 import com.in28minutes.courses.GetCourseDetailsResponse;
+import com.soap.bean.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -10,14 +14,23 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class CourseDetailsEndpoint {
+
+    @Autowired
+    CourseDetailsService service;
+
     @PayloadRoot(namespace = "http://in28minutes.com/courses", localPart = "GetCourseDetailsRequest")
     @ResponsePayload
     public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+        Course course = service.findById(request.getId());
+        return mapCourse(course);
+    }
+
+    private GetCourseDetailsResponse mapCourse(Course course) {
         GetCourseDetailsResponse response = new GetCourseDetailsResponse();
         CourseDetails courseDetails = new CourseDetails();
-        courseDetails.setId(request.getId());
-        courseDetails.setName("Microservices Course");
-        courseDetails.setDescription("That would be a great course");
+        courseDetails.setId(course.getId());
+        courseDetails.setName(course.getName());
+        courseDetails.setDescription(course.getDescription());
         response.setCourseDetails(courseDetails);
         return response;
     }
